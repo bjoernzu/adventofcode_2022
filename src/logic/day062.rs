997b49d35c1513b4821ac95214ead6dd2df70339
Bjoern Zucha <Zucha.Bjoern@bcgplatinion.com>
2022-12-07 08:37:07 +0100

    Day 6 Part 2

--- src/logic/day062.rs
+++ src/logic/day062.rs
@@ -1,20 +1,31 @@
 use crate::read_input;
+use std::collections::HashSet;
 
 pub struct Day062;
 impl Day062 {
 
-    pub fn run(&self) -> &str {
+    pub fn run(&self) -> usize {
         let filename = "input/day061.txt";
         let input = read_input(filename);
-    
-        for line in input.lines() {
-
+        
+        let mut result :usize = 0;
+        let marker_size = 14;
+        for i in marker_size..=input.len() -1 {
+            if is_unique(input.get(i-marker_size..i).unwrap()) {
+                result = i;
+                break;
+            }
         }
         
-        let result = "";
         // Print the result
         println!("Day 06 - Part 2: Result is {}", result);
 
         return result
     }
+}
+
+fn is_unique(str: &str) -> bool {
+    let r = str.chars().collect::<HashSet<char>>().len() == str.len();
+    // println!("{} is unique: {}", str, r);
+    return r;
 }
