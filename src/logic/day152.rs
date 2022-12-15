78cf76b53067a1f5db977d6d94060f0eb14ff618
Bjoern Zucha <Zucha.Bjoern@bcgplatinion.com>
2022-12-15 12:53:39 +0100

    Day 15 - Part 2 - Optimized, but still not super fast

--- src/logic/day152.rs
+++ src/logic/day152.rs
@@ -1,4 +1,4 @@
-use std::cmp::{max, min};
+use std::{cmp::{max, min}, collections::HashSet};
 
 use crate::read_input;
 
@@ -8,7 +8,24 @@ struct Sensor {
     distance: i64
 }
 
+impl Sensor {
+    fn new (x: i64,y: i64,distance: i64) -> Sensor {
+        Sensor {
+            x,
+            y,
+            distance
+        }
+    }
 
+    fn range_at_y(&self, y: i64, minx: i64, maxx: i64) -> (i64, i64) {
+        if (self.y - y).abs() > self.distance {
+            return (0,0); // If the empty spot is at the first position of a line returninh (0,0) will fail the whole algorithm
+        }
+        let x_diff = self.distance - (self.y - y).abs();
+        let range = (max(minx, self.x - x_diff), min(maxx, self.x + x_diff));
+        return range;
+    }
+}
 
 pub struct Day152;
 impl Day152 {
@@ -17,7 +34,6 @@ impl Day152 {
         let input = read_input(filename);
 
         let mut sensors = Vec::new();
-        let mut beacons = Vec::new();
 
         // Need to store the smallest and largest x and y
         let minx = 0;
@@ -29,7 +45,6 @@ impl Day152 {
             let parts: Vec<&str> = line.split(": closest beacon is at x=").collect();
 
             let beacon_xy: Vec<i64> = parts[1].split(", y=").map(|s| s.parse().unwrap()).collect();
-            beacons.push((beacon_xy[0], beacon_xy[1]));
 
             let sensor_parts: Vec<&str> = parts[0].split("Sensor at x=").collect();
             let sensor_xy: Vec<i64> = sensor_parts[1]
@@ -38,16 +53,26 @@ impl Day152 {
                 .collect();
             let distance =
                 (beacon_xy[0] - sensor_xy[0]).abs() + (beacon_xy[1] - sensor_xy[1]).abs();
-            sensors.push((sensor_xy[0], sensor_xy[1], distance));
+            sensors.push(Sensor::new(sensor_xy[0], sensor_xy[1], distance));
 
         }
 
         // Build the empty cave
         for y in miny..=maxy {
-            for x in minx..=maxx {
-                if !sensors.iter().any(|s| (s.0-x).abs() + (s.1-y).abs() <= s.2) {
+            let mut ranges: Vec<(i64, i64)> = sensors.iter().map(|s| s.range_at_y(y, minx, maxx)).collect();
+            ranges.retain(|&r| r != (0,0));
+            ranges.sort_by(|a,b| a.0.partial_cmp(&b.0).unwrap());
+            let mut coverage = ranges.remove(0);
+            
+            for range in ranges {
+                if overlap(&range, &coverage) {
+                    // println!("{},{} and {}, {} overlap", range.0, range.1, coverage.0, coverage.1);
+                    coverage.1 = max(coverage.1,range.1);
+                }
+                else {
+                    println!("{},{} and {}, {} do not overlap in line {}", range.0, range.1, coverage.0, coverage.1, y);
                     // Print the result
-                    let result = x * 4000000 + y;
+                    let result = (coverage.1 + 1) * 4000000 + y;
                     println!("Day 15 - Part 2: Result is {}", result);
                     return
                 }
@@ -57,3 +82,6 @@ impl Day152 {
     }
 }
 
+fn overlap(r1: &(i64, i64), r2: &(i64, i64)) -> bool {
+    !(r1.0 > r2.1 || r1.1 < r2.0)
+}
